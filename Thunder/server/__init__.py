a4b6c5263fda316037ff84d4354b3c5d01a78521
TahirDea <tahirdea99@mynes.com>
2024-10-19 16:55:00 +0530

    Update __init__.py

--- Thunder/server/__init__.py
+++ Thunder/server/__init__.py
@@ -3,6 +3,14 @@ from .stream_routes import routes
 
 
 async def web_server():
-    web_app = web.Application(client_max_size=30000000)
+    """
+    Initializes the aiohttp web application with the necessary routes,
+    custom middleware, and configures the maximum request body size.
+
+    Returns:
+        web.Application: The aiohttp web application instance.
+    """
+    # Create web application with custom middleware and set max client request size to 30 MB
+    web_app = web.Application(middlewares=[custom_404_handler], client_max_size=30 * 1024 * 1024)
     web_app.add_routes(routes)
     return web_app
